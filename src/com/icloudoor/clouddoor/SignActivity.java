6c55f914a8117a30494b5e4907414e1b37c6d1e2
lif <lif@icloudoor.com>
2015-05-14 18:19:23 +0800

    增加忘记手势，管理手势功能

--- src/com/icloudoor/clouddoor/SignActivity.java
+++ src/com/icloudoor/clouddoor/SignActivity.java
@@ -145,6 +145,10 @@ public class SignActivity extends Activity{
 		
 		Log.e("Test Sign", "onResume");
 
+		
+		SharedPreferences setSign = getSharedPreferences("SETSIGN", 0);
+		haveSet = setSign.getInt("HAVESETSIGN", 0);
+		
 		if(haveSet == 1){
 			setContentView(R.layout.set_detail_set_sign_now);
 
@@ -199,20 +203,173 @@ public class SignActivity extends Activity{
 				@Override
 				public void onClick(View v) {
 					Log.e("TEsT", "changeSign clicked!");
+					Intent confirmIntent=new Intent(SignActivity.this,ConfirmGestureActivity.class);
+					startActivity(confirmIntent);
 				}
 				
 			});
 			
-			forgetSign.setOnClickListener(new OnClickListener(){
+			forgetSign.setOnClickListener(new OnClickListener() {
 
 				@Override
 				public void onClick(View v) {
 					Log.e("TEsT", "forgetSign clicked!");
+					MyDialog myDialog = new MyDialog(SignActivity.this, "µÇÂ¼ÃÜÂë",
+							new MyDialog.OnCustomDialogListener() {
+
+								@Override
+								public void back(int haveset) {
+									SignActivity.this.haveSet = haveset;
+									SharedPreferences setSign = getSharedPreferences(
+											"SETSIGN", 0);
+									Editor editor = setSign.edit();
+									editor.putInt("HAVESETSIGN",
+											SignActivity.this.haveSet);
+									editor.commit();
+									Log.e("forgetOncreate",
+											String.valueOf(haveSet));
+									useSign = 0;
+									SharedPreferences setting = getSharedPreferences(
+											"SETTING", MODE_PRIVATE);
+									Editor useSigneditor = setting.edit();
+									useSigneditor.putInt("useSign", useSign);
+									useSigneditor.commit();
+
+									setContentView(R.layout.set_detail_set_sign);
+									back = (RelativeLayout) findViewById(R.id.btn_back);
+									IvSignSwitch = (ImageView) findViewById(R.id.btn_sign_switch);
+
+									useSign = setting.getInt("useSign", 0);
+									if (useSign == 1)
+										IvSignSwitch
+												.setImageResource(R.drawable.btn_yes);
+									else
+										IvSignSwitch
+												.setImageResource(R.drawable.btn_no);
+									IvSignSwitch
+											.setOnClickListener(new android.view.View.OnClickListener() {
+
+												@Override
+												public void onClick(View v) {
+													// TODO Auto-generated
+													// method stub
+													if (useSign == 1) {
+														IvSignSwitch
+																.setImageResource(R.drawable.btn_no);
+														useSign = 0;
+
+														SharedPreferences setting = getSharedPreferences(
+																"SETTING",
+																MODE_PRIVATE);
+														Editor editor = setting
+																.edit();
+														editor.putInt(
+																"useSign",
+																useSign);
+														editor.commit();
+													} else {
+														IvSignSwitch
+																.setImageResource(R.drawable.btn_yes);
+														useSign = 1;
+
+														SharedPreferences setting = getSharedPreferences(
+																"SETTING",
+																MODE_PRIVATE);
+														Editor editor = setting
+																.edit();
+														editor.putInt(
+																"useSign",
+																useSign);
+														editor.commit();
+
+														if (haveSet == 0) {
+															Intent intent = new Intent();
+															intent.setClass(
+																	SignActivity.this,
+																	SetGestureActivity.class);
+															startActivity(intent);
+														}
+													}
+												}
+
+											});
+
+									back.setOnClickListener(new android.view.View.OnClickListener() {
+
+										@Override
+										public void onClick(View v) {
+											finish();
+										}
+
+									});
+
+								}
+							});
+
+					// SignActivity.this.onPause();
+					myDialog.show();
+				}
+
+			});
+			
+		
+		}else if(haveSet==0) {
+			setContentView(R.layout.set_detail_set_sign);
+			back = (RelativeLayout) findViewById(R.id.btn_back);
+			IvSignSwitch = (ImageView) findViewById(R.id.btn_sign_switch);
+			
+			SharedPreferences setting = getSharedPreferences(
+					"SETTING", MODE_PRIVATE);
+			useSign = setting.getInt("useSign", 0);
+			if(useSign == 1)
+				IvSignSwitch.setImageResource(R.drawable.btn_yes);
+			else
+				IvSignSwitch.setImageResource(R.drawable.btn_no);
+			IvSignSwitch.setOnClickListener(new android.view.View.OnClickListener(){
+				
+				@Override
+				public void onClick(View v) {
+					// TODO Auto-generated method stub
+					if(useSign == 1){
+						IvSignSwitch.setImageResource(R.drawable.btn_no);
+						useSign = 0;
+						
+						SharedPreferences setting = getSharedPreferences("SETTING",
+								MODE_PRIVATE);
+						Editor editor = setting.edit();
+						editor.putInt("useSign", useSign);
+						editor.commit();
+					}else{
+						IvSignSwitch.setImageResource(R.drawable.btn_yes);
+						useSign = 1;
+						
+						SharedPreferences setting = getSharedPreferences("SETTING",
+								MODE_PRIVATE);
+						Editor editor = setting.edit();
+						editor.putInt("useSign", useSign);
+						editor.commit();
+						
+						if(haveSet == 0	) 
+						{
+							Intent intent = new Intent();
+							intent.setClass(SignActivity.this, SetGestureActivity.class);
+							startActivity(intent);
+						}
+					}
 				}
 				
 			});
 			
-		
+			back.setOnClickListener(new android.view.View.OnClickListener(){
+
+				@Override
+				public void onClick(View v) {
+					finish();
+				}
+				
+			});
+			
+			
 		}
 	}
 	
